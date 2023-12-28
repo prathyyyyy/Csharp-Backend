1f11b1a7dd5a5e19ae71662330e26c6c277375aa
prathyyyyy <itsmeprathy10@gmail.com>
2023-12-28 15:05:11 +0530

    Shift Files
    Moving the backend files to a folder for structured view

---
+++ WEB-API/Repository/IRepository/ICountryRepository.cs
@@ -0,0 +1,11 @@
+﻿using WorldAPI.Data;
+using WorldAPI.Models;
+
+namespace WorldAPI.Repository.IRepository
+{
+    public interface ICountryRepository : IGenericRepository<Countries>
+    { 
+        Task Update (Countries entity);
+      
+    }
+}
